a5d1424f3fab4f9a8b78f22703e2c39be6943145
Aliyan Aslam <alynx957@protonmail.com>
2022-08-31 19:54:05 +0500

    Added Dashboard & Extension

---
+++ Dashboard/js/login.js
@@ -0,0 +1,43 @@
+function complete() {
+  tl.to(".anim", { clearProps: "all" });
+}
+
+const tl = gsap.timeline();
+
+tl.from(".loader span", {
+  y: 40,
+  autoAlpha: 0,
+  stagger: 0.1,
+});
+
+tl.to(".loader span", {
+  y: -40,
+  autoAlpha: 0,
+  stagger: 0.1,
+  delay: 1,
+});
+
+tl.to(".loader", {
+  scaleY: 0,
+  transformOrigin: "top",
+  ease: Expo.easeInOut,
+  duration: 1,
+});
+
+tl.to(
+  ".loader-back",
+  {
+    scaleY: 0,
+    transformOrigin: "bottom",
+    ease: Expo.easeInOut,
+    duration: 1,
+  },
+  "-=.1"
+);
+
+tl.from(".anim", {
+  autoAlpha: 0,
+  y: 20,
+  stagger: 0.1,
+  onComplete: complete,
+});
